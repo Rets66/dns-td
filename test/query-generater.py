63ec5e5ef9bb1211b35704bd4f3d818100255f11
Rets66 <asce_salut666chere@icloud.com>
2020-01-14 23:05:12 +0900

    Add

--- test/query-generater.py
+++ test/query-generater.py
@@ -3,11 +3,45 @@
 
 import random
 import string
+import json
 
-def gen_label(length):
-    return "".join(random.choices(string.ascii_lowercase + string.digits, k=length))
+TLD = ['com', 'net', 'org', 'uk', 'info', 'de']
 
-def main():
-    label_length = random.choice(range(1,64))
-    label = gen_label(label_length)
-    domain = ".".join(label)
+class DomainGenerater():
+    def __init__(self):
+        self.label_length = list(range(1, 64))
+        self.label_number = list(range(2, 6))
+        self.domain_list = []
+
+    def main(self, number):
+        """Generate random domain name list"""
+        hostname_list = []
+        for i in range(number):
+            length = random.choice(self.label_length)
+            hostname = self.gen_label(length)
+            hostname_list.append(hostname)
+        for i in range(number):
+            num = random.choice(self.label_number)
+            candidate = random.choices(hostname_list, k=num)
+            domain_name = self.gen_domain(candidate)
+            self.domain_list.append(domain_name)
+        return self.domain_list #["", "",..., ""]
+
+    def gen_label(self, length):
+        """Generate random domain label"""
+        characters = string.ascii_lowercase + string.digits
+        selected_charalist = random.choices(characters, k=length)
+        return "".join(selected_charalist) # return like as '3h5iyrcp7jbj'
+
+    def gen_domain(self, labels):
+        """Generate domain name from random label"""
+        tld = random.choice(TLD)
+        labels.append(tld)
+        return ".".join(labels)
+
+if __name__ == '__main__':
+    # intance
+    A = DomainGenerater()
+    name_list = str(A.main(10))
+    with open("domain_list.json", "wt") as f:
+        f.write(name_list)
